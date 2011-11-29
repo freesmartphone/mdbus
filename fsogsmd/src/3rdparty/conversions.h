76799b0459ada24e93e923f949a28763b12f4d6f
Simon Busch <morphis@gravedo.de>
2011-11-29 20:47:42 +0100

    fsogsmd: 3rdparty: add missing prototypes
    This is based on a patch from Colin Watson <cjwatson@ubuntu.com> attached to FSO bug
    report #656. It's also tracked in debian bug report system as #648447. Both bugs are fixed
    with this commit.
    
    Signed-off-by: Simon Busch <morphis@gravedo.de>

--- fsogsmd/src/3rdparty/conversions.h
+++ fsogsmd/src/3rdparty/conversions.h
@@ -1,12 +1,17 @@
 #ifndef CONVERSIONS_H
 #define CONVERSIONS_H
 
+char *utf8_to_ucs2(const char* str);
+char *utf8_to_gsm(const char* str);
 char *gsm_to_utf8(const char* str);
 char *ucs2_to_utf8(const char *str);
 typedef struct sms structsms;
+void sms_copy(void* self, void* dup);
 struct sms* sms_new();
 void sms_free(struct sms* self);
 long sms_size();
+struct cbs* cbs_new();
+void cbs_free(struct cb* self);
 
 #endif /* CONVERSIONS_H */
 
