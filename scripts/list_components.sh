ddb3d12bd993ee214d1a6bd3256142296d4f9920
Simon Busch <morphis@gravedo.de>
2012-04-14 10:06:10 +0200

    scripts: update release_distcheck.sh script with individual configure options for some components

--- scripts/list_components.sh
+++ scripts/list_components.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-COMPONENTS="libfsosystem libfsobasics libfsoframework libfsoresource fsousaged fsogsmd fsodeviced fsodatad fsonetworkd fsosystemd"
+COMPONENTS="libfsosystem libfsobasics libfsoframework libfsoresource fsousaged fsogsmd fsodeviced fsodatad fsonetworkd fsosystemd fsotdld"
--- scripts/release_distcheck.sh
+++ scripts/release_distcheck.sh
@@ -6,10 +6,24 @@ for comp in $COMPONENTS; do
 	pushd $comp
 	if [ ! -f ".skip_distcheck" ] ; then
 		if [ -f "Makefile" ] ; then
-			sudo make distclean
+			sudo make maintainer-clean
 		fi
+
 		./autogen.sh
-		make distcheck || exit 1
+
+		if [ $comp == "fsogsmd" ] ; then
+			./configure --enable-libgsm0710mux --enable-modem-qualcomm-palm \
+				--enable-modem-nokia-isi --enable-modem-samsung
+		elif [ $comp == "fsodeviced" ] ; then
+			./configure --enable-kernel26-rfkill --enable-player-canberra \
+				--enable-player-gstreamer
+		elif [ $comp == "fsoaudiod" ] ; then
+			./configure --enable-cmtspeechdata --enable-samplerate
+		fi
+
+		make || exit 1
+		sudo make distcheck || exit 1
+		touch .skip_distcheck
 	fi
 	popd
 done
