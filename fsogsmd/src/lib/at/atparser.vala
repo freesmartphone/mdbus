freesmartphone/cornucopia

fsogsmd: adjust for recent changes to libfsotransport about delegate handling

fsogsmd/src/lib/at/atparser.vala
@@ -162,7 +162,7 @@ public class FsoGsm.StateBasedAtParser : FsoFramework.BaseParser
                 return State.INLINE;
         }
 
-        if ( haveCommand() )
+        if ( _delegate.haveCommand() )
         {
             switch (c)
             {
@@ -323,7 +323,7 @@ public class FsoGsm.StateBasedAtParser : FsoFramework.BaseParser
         debug( "line completed: '%s'", (string)curline );
 #endif
 
-        if ( !haveCommand() )
+        if ( !_delegate.haveCommand() )
         {
             return endoflineSurelyUnsolicited();
         }
@@ -359,10 +359,10 @@ public class FsoGsm.StateBasedAtParser : FsoFramework.BaseParser
         }
 
 #if DEBUG
-        var prefixExpected = expectedPrefix( (string)curline );
+        var prefixExpected = _delegate.isExpectedPrefix( (string)curline );
         debug( "prefix expected = %s", prefixExpected.to_string() );
 #endif
-        if ( !expectedPrefix( (string)curline ) )
+        if ( !_delegate.isExpectedPrefix( (string)curline ) )
         {
             return endoflineSurelyUnsolicited();
         }
@@ -381,7 +381,7 @@ public class FsoGsm.StateBasedAtParser : FsoFramework.BaseParser
 #if DEBUG
         debug( "is final response. solicited response with %d lines", solicited.length );
 #endif
-        solicitedCompleted( solicited ); //TODO: rather call in idle mode or will this confuse everything?
+        _delegate.solicitedCompleted( solicited ); //TODO: rather call in idle mode or will this confuse everything?
         return resetAll();
     }
 
@@ -398,7 +398,7 @@ public class FsoGsm.StateBasedAtParser : FsoFramework.BaseParser
             debug( "pending PDU received; unsolicited response completed." );
 #endif
             pendingUnsolicitedPDU = false;
-            unsolicitedCompleted( unsolicited );
+            _delegate.unsolicitedCompleted( unsolicited );
             return resetAll( false );
         }
 
@@ -413,7 +413,7 @@ public class FsoGsm.StateBasedAtParser : FsoFramework.BaseParser
 #if DEBUG
         debug( "unsolicited response completed." );
 #endif
-        unsolicitedCompleted( unsolicited );
+        _delegate.unsolicitedCompleted( unsolicited );
         return resetAll( false );  // do not clear solicited responses; we might have sneaked in-between
     }
 

fsogsmd/src/plugins/modem_qualcomm_htc/htcparser.vala
@@ -165,7 +165,7 @@ public class FsoGsm.HtcAtParser : FsoFramework.BaseParser
                 return State.INLINE;
         }
 
-        if ( haveCommand() )
+        if ( _delegate.haveCommand() )
         {
             switch (c)
             {
@@ -345,7 +345,7 @@ public class FsoGsm.HtcAtParser : FsoFramework.BaseParser
         debug( "line completed: '%s'", (string)curline );
 #endif
 
-        if ( !haveCommand() )
+        if ( !_delegate.haveCommand() )
         {
             return endoflineSurelyUnsolicited();
         }
@@ -381,10 +381,10 @@ public class FsoGsm.HtcAtParser : FsoFramework.BaseParser
         }
 
 #if DEBUG
-        var prefixExpected = expectedPrefix( (string)curline );
+        var prefixExpected = _delegate.isExpectedPrefix( (string)curline );
         debug( "prefix expected = %s", prefixExpected.to_string() );
 #endif
-        if ( !expectedPrefix( (string)curline ) )
+        if ( !_delegate.isExpectedPrefix( (string)curline ) )
         {
             return endoflineSurelyUnsolicited();
         }
@@ -403,7 +403,7 @@ public class FsoGsm.HtcAtParser : FsoFramework.BaseParser
 #if DEBUG
         debug( "is final response. solicited response with %d lines", solicited.length );
 #endif
-        solicitedCompleted( solicited ); //TODO: rather call in idle mode or will this confuse everything?
+        _delegate.solicitedCompleted( solicited ); //TODO: rather call in idle mode or will this confuse everything?
         return resetAll();
     }
 
@@ -420,7 +420,7 @@ public class FsoGsm.HtcAtParser : FsoFramework.BaseParser
             debug( "pending PDU received; unsolicited response completed." );
 #endif
             pendingUnsolicitedPDU = false;
-            unsolicitedCompleted( unsolicited );
+            _delegate.unsolicitedCompleted( unsolicited );
             return resetAll( false );
         }
 
@@ -435,7 +435,7 @@ public class FsoGsm.HtcAtParser : FsoFramework.BaseParser
 #if DEBUG
         debug( "unsolicited response completed." );
 #endif
-        unsolicitedCompleted( unsolicited );
+        _delegate.unsolicitedCompleted( unsolicited );
         return resetAll( false );  // do not clear solicited responses; we might have sneaked in-between
     }
 

fsogsmd/tests/testatparser.vala
@@ -20,41 +20,44 @@
 using GLib;
 using FsoGsm;
 
-bool haveCommand = false;
+bool _haveCommand = false;
 bool expectedPrefix = false;
-
-public bool hcf()
-{
-    return haveCommand;
-}
-
-public bool epf()
-{
-    return expectedPrefix;
-}
 
 string[] solicitedResponse;
 string[] unsolicitedResponse;
 
-public void soli( string[] response )
+public class TestParserDelegate : FsoFramework.IParserDelegate, GLib.Object
 {
-    assert( haveCommand );
-    assert( solicitedResponse.length == response.length );
-    for ( int i = 0; i < response.length; ++i )
+    public bool haveCommand()
     {
-        debug( "line %d = '%s'", i, response[i] );
-        assert( response[i] == solicitedResponse[i] );
+        return _haveCommand;
     }
-}
 
-public void unsoli( string[] response )
-{
-    assert( !haveCommand || !expectedPrefix );
-    assert( unsolicitedResponse.length == response.length );
-    for ( int i = 0; i < response.length; ++i )
+    public bool isExpectedPrefix( string line )
     {
-        debug( "line %d = '%s'", i, response[i] );
-        assert( response[i] == unsolicitedResponse[i] );
+        return expectedPrefix;
+    }
+
+    public void solicitedCompleted( string[] response )
+    {
+        assert( _haveCommand );
+        assert( solicitedResponse.length == response.length );
+        for ( int i = 0; i < response.length; ++i )
+        {
+            debug( "line %d = '%s'", i, response[i] );
+            assert( response[i] == solicitedResponse[i] );
+        }
+    }
+
+    public void unsolicitedCompleted( string[] response )
+    {
+        assert( !_haveCommand || !expectedPrefix );
+        assert( unsolicitedResponse.length == response.length );
+        for ( int i = 0; i < response.length; ++i )
+        {
+            debug( "line %d = '%s'", i, response[i] );
+            assert( response[i] == unsolicitedResponse[i] );
+        }
     }
 }
 
@@ -63,9 +66,9 @@ void test_parser_1_solicited()
 //===========================================================================
 {
     FsoFramework.Parser parser = new StateBasedAtParser();
-    parser.setDelegates( hcf, epf, soli, unsoli );
+    parser.setDelegate( new TestParserDelegate() );
 
-    haveCommand = true;
+    _haveCommand = true;
     expectedPrefix = false; // irrelevant for terminal lines
     solicitedResponse = { "OK" };
     unsolicitedResponse = {};
@@ -77,9 +80,9 @@ void test_parser_1_unsolicited()
 //===========================================================================
 {
     FsoFramework.Parser parser = new StateBasedAtParser();
-    parser.setDelegates( hcf, epf, soli, unsoli );
+    parser.setDelegate( new TestParserDelegate() );
 
-    haveCommand = false;
+    _haveCommand = false;
     expectedPrefix = false;
     solicitedResponse = {};
     unsolicitedResponse = { "+FOO: Yo Kurt" };
@@ -91,9 +94,9 @@ void test_parser_2_solicited()
 //===========================================================================
 {
     FsoFramework.Parser parser = new StateBasedAtParser();
-    parser.setDelegates( hcf, epf, soli, unsoli );
+    parser.setDelegate( new TestParserDelegate() );
 
-    haveCommand = true;
+    _haveCommand = true;
     expectedPrefix = true;
     solicitedResponse = { "+CPIN: \"READY\"", "OK" };
     unsolicitedResponse = {};
@@ -106,9 +109,9 @@ void test_parser_2_unsolicited()
 //===========================================================================
 {
     FsoFramework.Parser parser = new StateBasedAtParser();
-    parser.setDelegates( hcf, epf, soli, unsoli );
+    parser.setDelegate( new TestParserDelegate() );
 
-    haveCommand = false;
+    _haveCommand = false;
     expectedPrefix = false;
     solicitedResponse = {};
     unsolicitedResponse = { "+FOO: Yo Kurt" };
@@ -123,9 +126,9 @@ void test_parser_2_unsolicited_pdu()
 //===========================================================================
 {
     FsoFramework.Parser parser = new StateBasedAtParser();
-    parser.setDelegates( hcf, epf, soli, unsoli );
+    parser.setDelegate( new TestParserDelegate() );
 
-    haveCommand = false;
+    _haveCommand = false;
     expectedPrefix = false;
     solicitedResponse = {};
     unsolicitedResponse = { "+CMT: 120", "1234567890" };
@@ -138,9 +141,9 @@ void test_parser_multiline_solicited()
 //===========================================================================
 {
     FsoFramework.Parser parser = new StateBasedAtParser();
-    parser.setDelegates( hcf, epf, soli, unsoli );
+    parser.setDelegate( new TestParserDelegate() );
 
-    haveCommand = true;
+    _haveCommand = true;
     expectedPrefix = true;
     solicitedResponse = { "+FOO: 123456", "+BAR: 123456", "OK" };
     unsolicitedResponse = {};

freesmartphone/mdbus

fsogsmd/src/lib/at/atparser.vala
@@ -162,7 +162,7 @@ public class FsoGsm.StateBasedAtParser : FsoFramework.BaseParser
                 return State.INLINE;
         }
 
-        if ( haveCommand() )
+        if ( _delegate.onParserHaveCommand() )
         {
             switch (c)
             {
@@ -323,7 +323,7 @@ public class FsoGsm.StateBasedAtParser : FsoFramework.BaseParser
         debug( "line completed: '%s'", (string)curline );
 #endif
 
-        if ( !haveCommand() )
+        if ( !_delegate.onParserHaveCommand() )
         {
             return endoflineSurelyUnsolicited();
         }
@@ -359,10 +359,10 @@ public class FsoGsm.StateBasedAtParser : FsoFramework.BaseParser
         }
 
 #if DEBUG
-        var prefixExpected = expectedPrefix( (string)curline );
+        var prefixExpected = _delegate.onParserIsExpectedPrefix( (string)curline );
         debug( "prefix expected = %s", prefixExpected.to_string() );
 #endif
-        if ( !expectedPrefix( (string)curline ) )
+        if ( !_delegate.onParserIsExpectedPrefix( (string)curline ) )
         {
             return endoflineSurelyUnsolicited();
         }
@@ -381,7 +381,7 @@ public class FsoGsm.StateBasedAtParser : FsoFramework.BaseParser
 #if DEBUG
         debug( "is final response. solicited response with %d lines", solicited.length );
 #endif
-        solicitedCompleted( solicited ); //TODO: rather call in idle mode or will this confuse everything?
+        _delegate.onParserSolicitedCompleted( solicited ); //TODO: rather call in idle mode or will this confuse everything?
         return resetAll();
     }
 
@@ -398,7 +398,7 @@ public class FsoGsm.StateBasedAtParser : FsoFramework.BaseParser
             debug( "pending PDU received; unsolicited response completed." );
 #endif
             pendingUnsolicitedPDU = false;
-            unsolicitedCompleted( unsolicited );
+            _delegate.onParserUnsolicitedCompleted( unsolicited );
             return resetAll( false );
         }
 
@@ -413,7 +413,7 @@ public class FsoGsm.StateBasedAtParser : FsoFramework.BaseParser
 #if DEBUG
         debug( "unsolicited response completed." );
 #endif
-        unsolicitedCompleted( unsolicited );
+        _delegate.onParserUnsolicitedCompleted( unsolicited );
         return resetAll( false );  // do not clear solicited responses; we might have sneaked in-between
     }
 

fsogsmd/src/plugins/modem_qualcomm_htc/htcparser.vala
@@ -165,7 +165,7 @@ public class FsoGsm.HtcAtParser : FsoFramework.BaseParser
                 return State.INLINE;
         }
 
-        if ( haveCommand() )
+        if ( _delegate.onParserHaveCommand() )
         {
             switch (c)
             {
@@ -345,7 +345,7 @@ public class FsoGsm.HtcAtParser : FsoFramework.BaseParser
         debug( "line completed: '%s'", (string)curline );
 #endif
 
-        if ( !haveCommand() )
+        if ( !_delegate.onParserHaveCommand() )
         {
             return endoflineSurelyUnsolicited();
         }
@@ -381,10 +381,10 @@ public class FsoGsm.HtcAtParser : FsoFramework.BaseParser
         }
 
 #if DEBUG
-        var prefixExpected = expectedPrefix( (string)curline );
+        var prefixExpected = _delegate.onParserIsExpectedPrefix( (string)curline );
         debug( "prefix expected = %s", prefixExpected.to_string() );
 #endif
-        if ( !expectedPrefix( (string)curline ) )
+        if ( !_delegate.onParserIsExpectedPrefix( (string)curline ) )
         {
             return endoflineSurelyUnsolicited();
         }
@@ -403,7 +403,7 @@ public class FsoGsm.HtcAtParser : FsoFramework.BaseParser
 #if DEBUG
         debug( "is final response. solicited response with %d lines", solicited.length );
 #endif
-        solicitedCompleted( solicited ); //TODO: rather call in idle mode or will this confuse everything?
+        _delegate.onParserSolicitedCompleted( solicited ); //TODO: rather call in idle mode or will this confuse everything?
         return resetAll();
     }
 
@@ -420,7 +420,7 @@ public class FsoGsm.HtcAtParser : FsoFramework.BaseParser
             debug( "pending PDU received; unsolicited response completed." );
 #endif
             pendingUnsolicitedPDU = false;
-            unsolicitedCompleted( unsolicited );
+            _delegate.onParserUnsolicitedCompleted( unsolicited );
             return resetAll( false );
         }
 
@@ -435,7 +435,7 @@ public class FsoGsm.HtcAtParser : FsoFramework.BaseParser
 #if DEBUG
         debug( "unsolicited response completed." );
 #endif
-        unsolicitedCompleted( unsolicited );
+        _delegate.onParserUnsolicitedCompleted( unsolicited );
         return resetAll( false );  // do not clear solicited responses; we might have sneaked in-between
     }
 

fsogsmd/tests/testatparser.vala
@@ -23,38 +23,41 @@ using FsoGsm;
 bool haveCommand = false;
 bool expectedPrefix = false;
 
-public bool hcf()
-{
-    return haveCommand;
-}
-
-public bool epf()
-{
-    return expectedPrefix;
-}
-
 string[] solicitedResponse;
 string[] unsolicitedResponse;
 
-public void soli( string[] response )
+public class TestParserDelegate : FsoFramework.IParserDelegate, GLib.Object
 {
-    assert( haveCommand );
-    assert( solicitedResponse.length == response.length );
-    for ( int i = 0; i < response.length; ++i )
+    public bool onParserHaveCommand()
     {
-        debug( "line %d = '%s'", i, response[i] );
-        assert( response[i] == solicitedResponse[i] );
+        return haveCommand;
     }
-}
 
-public void unsoli( string[] response )
-{
-    assert( !haveCommand || !expectedPrefix );
-    assert( unsolicitedResponse.length == response.length );
-    for ( int i = 0; i < response.length; ++i )
+    public bool onParserIsExpectedPrefix( string line )
     {
-        debug( "line %d = '%s'", i, response[i] );
-        assert( response[i] == unsolicitedResponse[i] );
+        return expectedPrefix;
+    }
+
+    public void onParserSolicitedCompleted( string[] response )
+    {
+        assert( haveCommand );
+        assert( solicitedResponse.length == response.length );
+        for ( int i = 0; i < response.length; ++i )
+        {
+            debug( "line %d = '%s'", i, response[i] );
+            assert( response[i] == solicitedResponse[i] );
+        }
+    }
+
+    public void onParserUnsolicitedCompleted( string[] response )
+    {
+        assert( !haveCommand || !expectedPrefix );
+        assert( unsolicitedResponse.length == response.length );
+        for ( int i = 0; i < response.length; ++i )
+        {
+            debug( "line %d = '%s'", i, response[i] );
+            assert( response[i] == unsolicitedResponse[i] );
+        }
     }
 }
 
@@ -63,7 +66,7 @@ void test_parser_1_solicited()
 //===========================================================================
 {
     FsoFramework.Parser parser = new StateBasedAtParser();
-    parser.setDelegates( hcf, epf, soli, unsoli );
+    parser.setDelegate( new TestParserDelegate() );
 
     haveCommand = true;
     expectedPrefix = false; // irrelevant for terminal lines
@@ -77,7 +80,7 @@ void test_parser_1_unsolicited()
 //===========================================================================
 {
     FsoFramework.Parser parser = new StateBasedAtParser();
-    parser.setDelegates( hcf, epf, soli, unsoli );
+    parser.setDelegate( new TestParserDelegate() );
 
     haveCommand = false;
     expectedPrefix = false;
@@ -91,7 +94,7 @@ void test_parser_2_solicited()
 //===========================================================================
 {
     FsoFramework.Parser parser = new StateBasedAtParser();
-    parser.setDelegates( hcf, epf, soli, unsoli );
+    parser.setDelegate( new TestParserDelegate() );
 
     haveCommand = true;
     expectedPrefix = true;
@@ -106,7 +109,7 @@ void test_parser_2_unsolicited()
 //===========================================================================
 {
     FsoFramework.Parser parser = new StateBasedAtParser();
-    parser.setDelegates( hcf, epf, soli, unsoli );
+    parser.setDelegate( new TestParserDelegate() );
 
     haveCommand = false;
     expectedPrefix = false;
@@ -123,7 +126,7 @@ void test_parser_2_unsolicited_pdu()
 //===========================================================================
 {
     FsoFramework.Parser parser = new StateBasedAtParser();
-    parser.setDelegates( hcf, epf, soli, unsoli );
+    parser.setDelegate( new TestParserDelegate() );
 
     haveCommand = false;
     expectedPrefix = false;
@@ -138,7 +141,7 @@ void test_parser_multiline_solicited()
 //===========================================================================
 {
     FsoFramework.Parser parser = new StateBasedAtParser();
-    parser.setDelegates( hcf, epf, soli, unsoli );
+    parser.setDelegate( new TestParserDelegate() );
 
     haveCommand = true;
     expectedPrefix = true;